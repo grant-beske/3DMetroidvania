1be3f7445faa5c0546547f7fd90542a201de99b6
Grant Beske <grantbeske@gmail.com>
2021-12-09 01:39:01 -0500

    Add scanning state to scan visor

--- Assets/Scripts/ScanVisor.cs
+++ Assets/Scripts/ScanVisor.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ScanVisor : MonoBehaviour {
 
-    enum State {NORMAL, VIEWSCAN}
+    enum State {NORMAL, SCANNING, VIEWSCAN}
     private State activeState = State.NORMAL;
 
     // UI controller
@@ -16,21 +16,53 @@ public class ScanVisor : MonoBehaviour {
     public AudioClip scanHighlightSound;
     public AudioClip scanViewScanSound;
 
-    // Scan state gameobjects
-    public GameObject normalStateObj;
-    public GameObject viewScanStateObj;
+    // Scanning state object containers
+    public NormalStateVars normalStateVars;
+    public ScanningStateVars scanningStateVars;
+    public ViewScanStateVars viewScanStateVars;
 
-    // Scan crosshair gameobjects
-    public GameObject scanTargetBlank;
-    public GameObject scanTargetHighlighted;
-    public GameObject scanTargetSelected;
-    private bool targetIsHighlighted = false;
+    [System.Serializable]
+    public class NormalStateVars {
+        // Main state gameobject
+        public GameObject stateObj;
 
-    // View scan gameobjects
-    [SerializeField] Text scanDescription;
+        // Scan crosshair gameobjects
+        public GameObject scanTargetBlank;
+        public GameObject scanTargetHighlighted;
+        [HideInInspector] public bool targetIsHighlighted = false;
+    }
+
+    [System.Serializable]
+    public class ScanningStateVars {
+        // Main state gameobject
+        public GameObject stateObj;
+
+        // UI gameobjects
+        [SerializeField] Text scanningText;
+        public GameObject progressBar;
+
+        // Scanning state objects
+        public float timeRequiredToScan = 1.0f;
+        [HideInInspector] public GameObject scanGroup;
+        [HideInInspector] public float timeElapsed = 0.0f;
+    }
+
+    [System.Serializable]
+    public class ViewScanStateVars {
+        // Main state gameobject
+        public GameObject stateObj;
+
+        // UI gameobjects
+        [SerializeField] public Text scanDescription;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    // Class Functions
+    /////////////////////////////////////////////////////////////////////////////////////
 
     void Start() {
         InitializeNormalState();
+        InitializeScanningState();
         InitializeViewScanState();
         uiController = userInterface.GetComponent<UserInterface>();
     }
@@ -48,7 +80,7 @@ public class ScanVisor : MonoBehaviour {
                         (1 << 7) | (1 << 10) | (1 << 11) | (1 << 12),
                         QueryTriggerInteraction.Collide)) {
                     if (hit.collider.gameObject.layer >= 10 && hit.collider.gameObject.layer <= 12) {
-                        SwitchToViewScanState(hit);
+                        SwitchToScanningState(hit);
                     }
                 }
             } else {
@@ -70,6 +102,39 @@ public class ScanVisor : MonoBehaviour {
                     ToggleScanHighlight(false);
                 }
             }
+        } else if (activeState == State.SCANNING) {
+            if (Input.GetMouseButton(0)) {
+                RaycastHit hit;
+                if (Physics.Raycast(
+                        Camera.main.transform.position,
+                        Camera.main.transform.forward,
+                        out hit,
+                        Mathf.Infinity,
+                        // Layer 7: Terrain, Layer 10-12: Scannable
+                        (1 << 7) | (1 << 10) | (1 << 11) | (1 << 12),
+                        QueryTriggerInteraction.Collide)) {
+                    if (hit.collider.gameObject.layer >= 10 && hit.collider.gameObject.layer <= 12) {
+                        GameObject hitScanGroup =
+                            hit.collider.gameObject.GetComponent<Scannable>().parentScanGroupObj;
+                        if (hitScanGroup == scanningStateVars.scanGroup) {
+                            scanningStateVars.timeElapsed += Time.deltaTime;
+                            UpdateProgressBar();
+                            // TODO - scanning sound
+                            if (scanningStateVars.timeElapsed >= scanningStateVars.timeRequiredToScan) {
+                                SwitchToViewScanState(hit);
+                            }
+                        } else {
+                            SwitchToNormalState();
+                        }
+                    } else {
+                        SwitchToNormalState();
+                    }
+                } else {
+                    SwitchToNormalState();
+                }
+            } else {
+                SwitchToNormalState();
+            }
         } else if (activeState == State.VIEWSCAN) {
             if (Input.GetMouseButtonDown(0)) {
                 SwitchToNormalState();
@@ -78,19 +143,23 @@ public class ScanVisor : MonoBehaviour {
     }
 
     private void InitializeNormalState() {
-        normalStateObj.SetActive(true);
-        scanTargetBlank.SetActive(true);
-        scanTargetHighlighted.SetActive(false);
-        scanTargetSelected.SetActive(false);
+        normalStateVars.stateObj.SetActive(true);
+        normalStateVars.scanTargetBlank.SetActive(true);
+        normalStateVars.scanTargetHighlighted.SetActive(false);
+    }
+
+    private void InitializeScanningState() {
+        scanningStateVars.stateObj.SetActive(false);
     }
 
     private void InitializeViewScanState() {
-        viewScanStateObj.SetActive(false);
+        viewScanStateVars.stateObj.SetActive(false);
     }
 
     private void SwitchToNormalState() {
-        viewScanStateObj.SetActive(false);
-        normalStateObj.SetActive(true);
+        viewScanStateVars.stateObj.SetActive(false);
+        scanningStateVars.stateObj.SetActive(false);
+        normalStateVars.stateObj.SetActive(true);
         activeState = State.NORMAL;
         uiController.DisableCursor();
         uiController.EnableGameControls();
@@ -99,16 +168,43 @@ public class ScanVisor : MonoBehaviour {
         Time.timeScale = 1f;
     }
 
+    private void SwitchToScanningState(RaycastHit hit) {
+        viewScanStateVars.stateObj.SetActive(false);
+        scanningStateVars.stateObj.SetActive(true);
+        normalStateVars.stateObj.SetActive(false);
+        activeState = State.SCANNING;
+        uiController.DisableCursor();
+        uiController.EnableGameControls();
+
+        // Time flows regularly in scanning mode.
+        Time.timeScale = 1f;
+
+        // Store information about what is being scanned. Allow for resumption of prev scan.
+        GameObject hitScanGroup =
+            hit.collider.gameObject.GetComponent<Scannable>().parentScanGroupObj;
+        if (scanningStateVars.scanGroup != hitScanGroup) {
+            scanningStateVars.scanGroup = hitScanGroup;
+            scanningStateVars.timeElapsed = 0.0f;
+            ResetProgressBar();
+        }
+
+        // If the hit scan group was already scanned, just directly go to VIEWSCAN state.
+        if (hitScanGroup.GetComponent<ScanGroup>().GetActiveState() == ScanGroup.State.SCANNED) {
+            SwitchToViewScanState(hit);
+        }
+    }
+
     private void SwitchToViewScanState(RaycastHit hit) {
-        normalStateObj.SetActive(false);
-        viewScanStateObj.SetActive(true);
+        normalStateVars.stateObj.SetActive(false);
+        scanningStateVars.stateObj.SetActive(false);
+        viewScanStateVars.stateObj.SetActive(true);
         activeState = State.VIEWSCAN;
         uiController.EnableCursor();
         uiController.DisableGameControls();
 
         // Set scan interface based on item scanned
         Scannable scan = hit.collider.gameObject.GetComponent<Scannable>();
-        scanDescription.text = scan.GetScan();
+        viewScanStateVars.scanDescription.text = scan.GetScan();
 
         // Play view scan sound effect
         PlaySound(scanViewScanSound);
@@ -121,20 +217,30 @@ public class ScanVisor : MonoBehaviour {
     }
 
     private void ToggleScanHighlight(bool shouldHighlight) {
-        if (targetIsHighlighted) {
+        if (normalStateVars.targetIsHighlighted) {
             if (!shouldHighlight) {
-                scanTargetHighlighted.SetActive(false);
-                scanTargetBlank.SetActive(true);
-                targetIsHighlighted = false;
+                normalStateVars.scanTargetHighlighted.SetActive(false);
+                normalStateVars.scanTargetBlank.SetActive(true);
+                normalStateVars.targetIsHighlighted = false;
             }
         } else {
             if (shouldHighlight) {
-                scanTargetBlank.SetActive(false);
-                scanTargetHighlighted.SetActive(true);
-                targetIsHighlighted = true;
+                normalStateVars.scanTargetBlank.SetActive(false);
+                normalStateVars.scanTargetHighlighted.SetActive(true);
+                normalStateVars.targetIsHighlighted = true;
                 PlaySound(scanHighlightSound);
             }
         }
+    }
+
+    private void ResetProgressBar() {
+        scanningStateVars.progressBar.transform.localScale = new Vector3(0, 1, 1);
+    }
+
+    private void UpdateProgressBar() {
+        float percentComplete =
+            scanningStateVars.timeElapsed / scanningStateVars.timeRequiredToScan;
+        scanningStateVars.progressBar.transform.localScale = new Vector3(percentComplete, 1, 1);
     }
 
     private void PlaySound(AudioClip clip) {

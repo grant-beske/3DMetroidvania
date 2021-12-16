16119aa795e8f827383292370c090fb8332e6891
Grant Beske <grantbeske@gmail.com>
2021-12-16 01:25:00 -0500

    Massively simplify weapons coordinator and add auto gun prefab

--- Assets/Scripts/Player/Weapons/WeaponsCoordinator.cs
+++ Assets/Scripts/Player/Weapons/WeaponsCoordinator.cs
@@ -5,56 +5,27 @@ using UnityEngine;
 public class WeaponsCoordinator : MonoBehaviour {
 
     // Gun control state.
-    // ACTIVE - gun is onscreen and ready to be fired.
-    // TRANSITIONING - gun is being opened / closed and is moving.
-    // INACTIVE - gun is offscreen and cannot be fired.
-    enum State {ACTIVE, TRANSITIONING, INACTIVE};
+    // ACTIVE - gun is onscreen and ready to be fired. Can switch weapons.
+    // INACTIVE - gun is offscreen and can't be fired. Happens in other visors like scan.
+    enum State {ACTIVE, INACTIVE};
     private State activeState = State.ACTIVE;
-
-    // Transitioning state requires a target transform to move to.
-    private Transform transitionEndPos;
-    private Transform transitionStartPos;
-    private Vector3 transitionDifference;
-    private State transitionTargetState;
-    public float transitionTimeSec = 0.25f;
-    private float transitionTimeElapsed = 0.0f;
 
     // Gun objects.
     public GameObject gunObject;
 
-    // Active and inactive gun positions.
-    public GameObject gunActiveObj;
-    private Transform gunActivePosition;
-    public GameObject gunInactiveObj;
-    private Transform gunInactivePosition;
-
-    // TODO - generalize this interface to multiple guns.
-
-    void Start() {
-        gunInactivePosition = gunInactiveObj.transform;
-        gunActivePosition = gunActiveObj.transform;
-    }
+    // Gun transitioning variables.
+    public Transform gunActivePosition;
+    public Transform gunInactivePosition;
+    public float transitionTimeSec = 0.25f;
 
     void Update() {
-        if (activeState == State.TRANSITIONING) {
-            UpdateTransitioningState();
+        if (activeState == State.ACTIVE) {
+            UpdateActiveState();
         }
     }
 
-    private void UpdateTransitioningState() {
-        if (transitionTimeElapsed < transitionTimeSec) {
-            transitionTimeElapsed += Time.deltaTime;
-            float percentComplete = transitionTimeElapsed / transitionTimeSec;
-            gunObject.transform.position =
-                transitionStartPos.position + (percentComplete * transitionDifference);
-        } else {
-            // Transition has reached target state. Finalize transition.
-            gunObject.transform.position = transitionEndPos.position;
-            if (transitionTargetState == State.ACTIVE)
-                gunObject.GetComponent<BaseGun>().EnableControl();
-            else gunObject.SetActive(false);
-            activeState = transitionTargetState;
-        }
+    private void UpdateActiveState() {
+        // TODO - generalize this interface to multiple guns.
     }
 
     /////////////////////////////////////////////////////////////////////////////////////
@@ -66,31 +37,53 @@ public class WeaponsCoordinator : MonoBehaviour {
 
     public void ActivateWeapon() {
         if (activeState != State.ACTIVE) {
-            // Transition the weapon to the ACTIVE state.
-            transitionTimeElapsed = 0.0f;
-            activeState = State.TRANSITIONING;
-            transitionTargetState = State.ACTIVE;
-            transitionEndPos = gunActivePosition;
-            transitionStartPos = gunInactivePosition;
-            transitionDifference = gunActivePosition.position - gunInactivePosition.position;
-            gunObject.SetActive(true);
+            StartCoroutine(TransitionToActiveState());
         } else {
             // If weapon was already active, just re-enable control.
             gunObject.GetComponent<BaseGun>().EnableControl();
         }
     }
 
+    private IEnumerator TransitionToActiveState() {
+        float timeElapsed = 0;
+        gunObject.transform.position = gunInactivePosition.position;
+        Vector3 transitionDifference =
+            gunActivePosition.position - gunInactivePosition.position;
+        gunObject.SetActive(true);
+        while (timeElapsed < transitionTimeSec) {
+            timeElapsed += Time.deltaTime;
+            float percentComplete = timeElapsed / transitionTimeSec;
+            gunObject.transform.position =
+                gunInactivePosition.position + (transitionDifference * percentComplete);
+            yield return null;
+        }
+        gunObject.transform.position = gunActivePosition.position;
+        gunObject.GetComponent<BaseGun>().EnableControl();
+        activeState = State.ACTIVE;
+    }
+
     public void DeactivateWeapon() {
         if (activeState != State.INACTIVE) {
-            // Transition the weapon to the INACTIVE state.
-            transitionTimeElapsed = 0.0f;
-            activeState = State.TRANSITIONING;
-            transitionTargetState = State.INACTIVE;
-            transitionEndPos = gunInactivePosition;
-            transitionStartPos = gunActivePosition;
-            transitionDifference = gunInactivePosition.position - gunActivePosition.position;
-            gunObject.GetComponent<BaseGun>().DisableControl();
+            StartCoroutine(TransitionToInactiveState());
         }
+    }
+
+    private IEnumerator TransitionToInactiveState() {
+        float timeElapsed = 0;
+        gunObject.transform.position = gunActivePosition.position;
+        Vector3 transitionDifference =
+            gunInactivePosition.position - gunActivePosition.position;
+        gunObject.GetComponent<BaseGun>().DisableControl();
+        while (timeElapsed < transitionTimeSec) {
+            timeElapsed += Time.deltaTime;
+            float percentComplete = timeElapsed / transitionTimeSec;
+            gunObject.transform.position =
+                gunActivePosition.position + (transitionDifference * percentComplete);
+            yield return null;
+        }
+        gunObject.transform.position = gunInactivePosition.position;
+        gunObject.SetActive(false);
+        activeState = State.INACTIVE;
     }
 
     // These functions are just for when we want to disable the firing of the gun.

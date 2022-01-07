f58e4ff42b6c229c1cee4c0e4cf8b7cd93f113b4
Grant Beske <grantbeske@gmail.com>
2022-01-06 21:54:56 -0500

    Ambiance controller crossfading

--- Assets/Scripts/Player/Core/AmbianceController.cs
+++ Assets/Scripts/Player/Core/AmbianceController.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class AmbianceController : MonoBehaviour {
 
     private string ambianceName;
-    private AudioClip ambianceClip;
-    private AudioSource audioSource;
+    private AudioClip ambianceClipCurrent;
+    private AudioClip ambianceClipPrev;
 
-    /////////////////////////////////////////////////////////////////////////////////////
-    // MonoBehavior
-    /////////////////////////////////////////////////////////////////////////////////////
+    public AudioSource audioSourceA;
+    public AudioSource audioSourceB;
+    private bool isSourceAActive = true;
 
-    void Start() {
-        audioSource = GetComponent<AudioSource>();
-    }
+    public float crossfadeTime = 0.3f;
 
     /////////////////////////////////////////////////////////////////////////////////////
     // Public API
@@ -23,10 +21,31 @@ public class AmbianceController : MonoBehaviour {
     public void SetAmbiance(AudioClip clip, string name) {
         if (ambianceName != name) {
             ambianceName = name;
-            audioSource.Stop();
-            ambianceClip = clip;
-            audioSource.clip = clip;
-            audioSource.Play();
+            ambianceClipPrev = ambianceClipCurrent;
+            ambianceClipCurrent = clip;
+            if (isSourceAActive) {
+                StartCoroutine(CrossfadeAmbiance(audioSourceA, audioSourceB));
+            } else {
+                StartCoroutine(CrossfadeAmbiance(audioSourceB, audioSourceA));
+            }
+            isSourceAActive = !isSourceAActive;
         }
+    }
+
+    private IEnumerator CrossfadeAmbiance(AudioSource fadeOut, AudioSource fadeIn) {
+        fadeIn.clip = ambianceClipCurrent;
+        fadeIn.volume = 0;
+        fadeIn.Play();
+
+        while (fadeIn.volume < 1.0f) {
+            float increment = Time.deltaTime / crossfadeTime;
+            fadeOut.volume -= increment;
+            fadeIn.volume += increment;
+            yield return null;
+        }
+
+        fadeIn.volume = 1.0f;
+        fadeOut.volume = 0.0f;
+        fadeOut.Stop();
     }
 }
